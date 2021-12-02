9c2d8e21a38756882d702e7b3a8428a3a0716923
Mohammad Jaha <miro.tune@gmail.com>
2021-12-02 05:31:00 +0000

    Uploading Ass

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let names = ["Miro","Mohammad","Reem","Lubabah","Shahad","Keven","Almin"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func coldCall(_ sender: Any) {
+        nameLabel.text = names[Int.random(in: 0..<names.count)]
+    }
+    
 }
 
